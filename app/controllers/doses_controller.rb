b6ba43c1f00379850528bd80be67338f0c70a215
Nancaidah Toure Chauvin <nancaidahct@gmail.com>
2020-08-13 17:44:08 +0200

    cocktails models, routes and view created, doses not finished

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,26 @@
+class DosesController < ApplicationController
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+    render 'new'
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktails_path
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:ingredient, :description)
+  end
+end
---
+++ db/migrate/20200813103715_add_ingredients_to_doses.rb
@@ -0,0 +1,4 @@
+class AddIngredientsToDoses < ActiveRecord::Migration[6.0]
+  def change
+  end
+end
